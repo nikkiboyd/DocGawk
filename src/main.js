6172c25483dae102c9197ba3496088b7cc0e7dc1
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-10 13:15:17 -0700

    add 'no doctor found' error message if no doctor's meet symptom criteria

--- src/main.js
+++ src/main.js
@@ -41,13 +41,17 @@ $(document).ready(function() {
         let promise = doctorService.getPortlandDoctorsBySymptom(symptom);
         promise.then(function(response) {
           let body = JSON.parse(response);
-          for (let i = 0; i < body.data.length; i++){
-            $('.doctor-matches').append(`<div class='card'>
-                                          <p>${body.data[i].practices[i].name}</p>
-                                          <p>Address: <br>${body.data[i].practices[i].visit_address.street}<br>${body.data[i].practices[i].visit_address.city}, ${body.data[i].practices[i].visit_address.state} ${body.data[i].practices[i].visit_address.zip}</p>
-                                          <p>Phone: <br> ${body.data[i].practices[i].phones[i].number}
-                                          <p>Accepts New Patients: ${body.data[i].practices[i].accepts_new_patients}
-                                          <p>Website: ${body.data[i].practices[i].website}`);
+          if (body.data.length > 0) {
+            for (let i = 0; i < body.data.length; i++){
+              $('.doctor-matches').append(`<div class='card'>
+                                            <p>${body.data[i].practices[i].name}</p>
+                                            <p>Address: <br>${body.data[i].practices[i].visit_address.street}<br>${body.data[i].practices[i].visit_address.city}, ${body.data[i].practices[i].visit_address.state} ${body.data[i].practices[i].visit_address.zip}</p>
+                                            <p>Phone: <br> ${body.data[i].practices[i].phones[i].number}
+                                            <p>Accepts New Patients: ${body.data[i].practices[i].accepts_new_patients}
+                                            <p>Website: ${body.data[i].practices[i].website}`);
+            }
+          } else if (body.data.length === 0) {
+            $('.errors').append('<p>No doctor found</p>');
           }
         }, function(error) {
           $('.errors').text(`There was an error processing your request: ${error.message}`);
