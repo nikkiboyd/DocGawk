13ff98506857647a6b57f9b0e29d09d5da08fba6
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-10 09:59:40 -0700

    able to retrieve doctor name from API

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,8 +1,8 @@
 class DoctorService {
-  getPortlandDoctors(firstName, lastName, symptom) {
+  getPortlandDoctors() {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=xavier&last_name=preciado&query=massage&location=or-portland&skip=0&limit=10&user_key=ff3483a6f4a38e5a6f5dbb31d03d5257`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
 import DoctorService from './doctor-service.js';
 
 $(document).ready(function() {
-  $('#find-doctor').click(function() {
-    let symptom = $('#symptom').val();
-    let firstName = $('#first-name').val();
-    let lastName = $('#last-name').val();
-    $('#first-name').val("");
-    $('#last-name').val("");
-    $('#symptom').val("");
+  $('#find-doctor').click(function(event) {
+    event.preventDefault();
+    // let symptom = $('#symptom').val();
+    // let firstName = $('#first-name').val();
+    // let lastName = $('#last-name').val();
 
       let doctorService = new DoctorService();
-      let promise = doctorService.getPortlandDoctors(firstName, lastName, symptom);
+      let promise = doctorService.getPortlandDoctors();
       promise.then(function(response) {
         let body = JSON.parse(response);
         for (let i = 0; i < body.data.length; i++){
-          $('.doctor-matches').append(`<li>test ${body.data.practices.name}</li>`);
+          console.log(body.data[i].practices[i]);
+          $('.doctor-matches').append(`<li>test ${body.data[i].practices[i].name}</li>`);
         }
       }, function(error) {
         $('.errors').text(`There was an error processing your request: ${error.message}`);
