5e66ba0496e189b441fe4b46b2d93188d60154f0
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-10 10:07:02 -0700

    remove semicolon from API key making it invalid and returning 401 error

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import DoctorService from './doctor-service.js';
 $(document).ready(function() {
   $('#find-doctor').click(function(event) {
     event.preventDefault();
-    // let symptom = $('#symptom').val();
-    // let firstName = $('#first-name').val();
-    // let lastName = $('#last-name').val();
+    let symptom = $('#symptom').val();
+    let firstName = $('#first-name').val();
+    let lastName = $('#last-name').val();
 
       let doctorService = new DoctorService();
-      let promise = doctorService.getPortlandDoctors();
+      let promise = doctorService.getPortlandDoctors(firstName, lastName, symptom);
       promise.then(function(response) {
         let body = JSON.parse(response);
         for (let i = 0; i < body.data.length; i++){
