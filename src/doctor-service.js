29d5507b0ae51dd664d8096dd34ab6f32fb571cc
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-10 10:44:31 -0700

    refactor code to include two separate API calls for searching for doctor by symptom OR name

--- src/doctor-service.js
+++ src/doctor-service.js
@@ -1,8 +1,24 @@
 class DoctorService {
-  getPortlandDoctors(firstName, lastName, symptom) {
+  getPortlandDoctorsByName(firstName, lastName) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?first_name=${firstName}&last_name=${lastName}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+
+  getPortlandDoctorsBySymptom(symptom) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}&location=or-portland&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -1,22 +1,38 @@
 import DoctorService from './doctor-service.js';
 
 $(document).ready(function() {
-  $('#find-doctor').click(function(event) {
+  $('#find-doctor-by-name').click(function(event) {
     event.preventDefault();
-    let symptom = $('#symptom').val();
     let firstName = $('#first-name').val();
     let lastName = $('#last-name').val();
 
       let doctorService = new DoctorService();
-      let promise = doctorService.getPortlandDoctors(firstName, lastName, symptom);
+      let promise = doctorService.getPortlandDoctorsByName(firstName, lastName);
       promise.then(function(response) {
         let body = JSON.parse(response);
         for (let i = 0; i < body.data.length; i++){
           console.log(body.data[i].practices[i]);
-          $('.doctor-matches').append(`<li>test ${body.data[i].practices[i].name}</li>`);
+          $('.doctor-matches').append(`<li>${body.data[i].practices[i].name}</li>`);
         }
       }, function(error) {
         $('.errors').text(`There was an error processing your request: ${error.message}`);
       });
     });
+
+    $('#find-doctor-by-symptom').click(function(event) {
+      event.preventDefault();
+      let symptom = $('#symptom').val();
+
+        let doctorService = new DoctorService();
+        let promise = doctorService.getPortlandDoctorsBySymptom(symptom);
+        promise.then(function(response) {
+          let body = JSON.parse(response);
+          for (let i = 0; i < body.data.length; i++){
+            console.log(body.data[i].practices[i]);
+            $('.doctor-matches').append(`<li>${body.data[i].practices[i].name}</li>`);
+          }
+        }, function(error) {
+          $('.errors').text(`There was an error processing your request: ${error.message}`);
+        });
+      });
 });
