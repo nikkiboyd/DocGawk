9a1fa7b8d1177bdb71874d74978782365db8dade
Nikki Boyd <boyd.nikki@icloud.com>
2018-08-10 14:37:19 -0700

    style error message page and hide/show div rather than append message

--- src/main.js
+++ src/main.js
@@ -20,15 +20,16 @@ $(document).ready(function() {
         console.log(body);
         if (body.data.length > 0) {
           for (let i = 0; i < body.data.length; i++){
-            $('.doctor-matches').append(`<div class='card'>
-                                          <p>${body.data[i].practices[i].name}</p>
-                                          <p>Address: <br>${body.data[i].practices[i].visit_address.street}<br>${body.data[i].practices[i].visit_address.city}, ${body.data[i].practices[i].visit_address.state} ${body.data[i].practices[i].visit_address.zip}</p>
-                                          <p>Phone: <br> ${body.data[i].practices[i].phones[i].number}
-                                          <p>Accepts New Patients: ${body.data[i].practices[i].accepts_new_patients}
-                                          <p>Website: ${body.data[i].practices[i].website}`);
+            $('.doctor-matches').append(`<div class='card center'>
+                                          <h4>${body.data[i].practices[i].name}</h4>
+                                          <h6>Address:</h6><p>${body.data[i].practices[i].visit_address.street}<br>${body.data[i].practices[i].visit_address.city}, ${body.data[i].practices[i].visit_address.state} ${body.data[i].practices[i].visit_address.zip}</p>
+                                          <h6>Phone:</h6><p>${body.data[i].practices[i].phones[i].number}
+                                          <h6>Accepts New Patients:</h6><p>${body.data[i].practices[i].accepts_new_patients}
+                                          <h6>Website:</h6><p>${body.data[i].practices[i].website}`);
           }
         } else if (body.data.length === 0) {
-          $('.errors').append('<p>No doctor found</p>');
+          $('.errors').fadeIn();
+          $('.errors').fadeOut(slow);
         }
       }, function(error) {
         console.log(error);
@@ -47,15 +48,15 @@ $(document).ready(function() {
           let body = JSON.parse(response);
           if (body.data.length > 0) {
             for (let i = 0; i < body.data.length; i++){
-              $('.doctor-matches').append(`<div class='card'>
-                                            <p>${body.data[i].practices[i].name}</p>
+              $('.doctor-matches').append(`<div class='card center'>
+                                            <h4>${body.data[i].practices[i].name}</h4>
                                             <p>Address: <br>${body.data[i].practices[i].visit_address.street}<br>${body.data[i].practices[i].visit_address.city}, ${body.data[i].practices[i].visit_address.state} ${body.data[i].practices[i].visit_address.zip}</p>
                                             <p>Phone: <br> ${body.data[i].practices[i].phones[i].number}
                                             <p>Accepts New Patients: ${body.data[i].practices[i].accepts_new_patients}
                                             <p>Website: ${body.data[i].practices[i].website}`);
             }
           } else if (body.data.length === 0) {
-            $('.errors').append('<p>No doctor found</p>');
+            $('.errors').append('<p>Sorry, no doctors match your search.</p>');
           }
         }, function(error) {
           $('.errors').text(`There was an error processing your request: ${error.message}`);
